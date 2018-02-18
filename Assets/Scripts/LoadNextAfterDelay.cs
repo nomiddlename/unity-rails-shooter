54b9c57e55c0f0523b9bf9dcf48f6b3cc7704764
Gareth Jones <gareth.jones@shinetech.com>
2018-02-18 13:49:05 +1100

    reorganised folders

---
+++ Assets/Scripts/LoadNextAfterDelay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextAfterDelay : MonoBehaviour
+{
+
+    [SerializeField] float delay;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("LoadNextScene", delay);
+    }
+
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
