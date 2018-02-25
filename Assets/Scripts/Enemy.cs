7d21fcd10e1d6d6d10d57bf5a9a76a7353181aa8
Gareth Jones <gareth.jones@shinetech.com>
2018-02-25 17:28:20 +1100

    add box collider at runtime

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,7 +9,13 @@ public class Enemy : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        AddNonTriggerBoxCollider();
+    }
 
+    private void AddNonTriggerBoxCollider()
+    {
+        BoxCollider collider = gameObject.AddComponent<BoxCollider>();
+        collider.isTrigger = false;
     }
 
     // Update is called once per frame
