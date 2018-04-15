d80153fb1091e7ad63dce9dcc98e8922eb813b21
Gareth Jones <gareth.jones@shinetech.com>
2018-04-15 20:59:04 +1000

    replaced path with timeline

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,6 +8,7 @@ public class Enemy : MonoBehaviour
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 10;
+    [SerializeField] int hits = 10;
 
     ScoreBoard scoreBoard;
 
@@ -32,9 +33,23 @@ public class Enemy : MonoBehaviour
 
     private void OnParticleCollision(GameObject other)
     {
+        ProcessHit();
+        if (hits < 1)
+        {
+            Death();
+        }
+    }
+
+    private void ProcessHit()
+    {
+        scoreBoard.ScoreHit(scorePerHit);
+        hits -= 1;
+    }
+
+    private void Death()
+    {
         GameObject explosion = Instantiate(deathFX, gameObject.transform.position, Quaternion.identity);
         explosion.transform.parent = parent;
-        scoreBoard.ScoreHit(scorePerHit);
         Destroy(gameObject);
     }
 }
